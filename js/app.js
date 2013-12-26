79cfd22de74127439a9f4903628b80ed39caff6d
Christo <jato@jatofire.com>
2013-12-26 18:20:18 +1100

    Minor update

--- js/app.js
+++ js/app.js
@@ -2,8 +2,31 @@
 
 function btnClick(index) {
 	if(index == 0) {
-		alert("You clicked List");
+		$("#list-table").show();
+		$("#add-form").hide();
 	} else {
-		alert("You clicked Add");
+		$("#add-form").show();
+		$("#list-table").hide();
 	}
 }
+
+
+var j ='[{"id":"1","name":"test1"},{"id":"2","name":"test2"},{"id":"3","name":"test3"},{"id":"4","name":"test4"},{"id":"5","name":"test5"}]';
+
+var json = $.parseJSON(j);
+
+//var to hold the html
+var st = "";
+
+for(var i = 0 ; i < 100; i++) {
+	
+	st += "<div class=\"list-table-item\"><div class=\"list-table-item-heading\">TOM</div>" + "BERENGER" + "</div>";
+
+
+}
+
+$("#list-table").html(st);
+
+
+
+
